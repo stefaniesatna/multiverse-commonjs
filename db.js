719bd460bf7a97bdb78859121239826a98e4a2cf
Stefanie Satna <heystef@fb.com>
2021-09-23 12:15:05 +0100

    Initialise a database

---
+++ db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize")
+const path = require("path")
+
+const dbPath = path.join(__dirname, "db.sqlite")
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: dbPath
+})
+
+module.exports = sequelize
